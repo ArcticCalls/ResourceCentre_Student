8eb1c7f9db1dd1b52801e5eae661b92d13752095
19020844 <19020844@DESKTOP-7S719PT>
2020-07-27 15:37:28 +0800

    Added Return for Camcorder and Chromebook

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -101,12 +101,46 @@ public class ResourceCentreTest {
 	public void doReturnCamcorderTest() {
 		//fail("Not yet implemented");
 		// write your code here
-		
+		ResourceCentre.viewAllCamcorder(camcorderList);
+		String assetTag = Helper.readString("Enter asset tag: ");
+		boolean isLoaned = false;
+		for (int i = 0; i < camcorderList.size(); i++) {
+			if (assetTag.equalsIgnoreCase(camcorderList.get(i).getAssetTag())) {
+				if (camcorderList.get(i).getIsAvailable() == false) {
+					String dueDate = Helper.readString("Enter due date: ");
+					camcorderList.get(i).setDueDate(dueDate);
+					camcorderList.get(i).setIsAvailable(true);
+					isLoaned = true;
+					System.out.println("Camcorder " + assetTag + " returned");
+				}
+				if (isLoaned == true) {
+					System.out.println("Invalid asset tag");
+				}
+			}
+		}
 	}
+	
 	@Test
 	public void doReturnChromebookTest() {
 		//fail("Not yet implemented");
 		// write your code here
+		ResourceCentre.viewAllChromebook(chromebookList);
+		String assetTag = Helper.readString("Enter asset tag: ");
+		boolean isLoaned = false;
+		for (int i = 0; i < chromebookList.size(); i++) {
+			if (assetTag.equalsIgnoreCase(chromebookList.get(i).getAssetTag())) {
+				if (chromebookList.get(i).getIsAvailable() == false) {
+					String dueDate = Helper.readString("Enter due date: ");
+					chromebookList.get(i).setDueDate(dueDate);
+					chromebookList.get(i).setIsAvailable(true);
+					isLoaned = true;
+					System.out.println("Chromebook " + assetTag + " returned");
+				}
+				if (isLoaned == true) {
+					System.out.println("Invalid asset tag");
+				}
+			}
+		}
 	}
 	
 	@After
